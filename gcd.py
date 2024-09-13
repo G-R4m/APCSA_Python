efa6c3f0844873fc0ffe28e56d3c6827b3cccfaa
Giancarlo Ramirez <giancarloramirez5@gmail.com>
2024-09-12 21:14:00 -0700

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,25 @@
+def gcd(a,b):
+    if a > b:
+        small = b
+        large = a
+    
+    else:
+        small = a
+        large = b
+        
+    r = large % small #this is the remainder
+    
+    while r > 0:
+        print (large, small, r)
+        large = small
+        small = r
+        r = small % r
+    
+    return small
+
+print (gcd (12345,67890222))
+        
+        
+        
+   
+    
