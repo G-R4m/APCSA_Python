0dec096091b03257748e5cb21aab0125582b4732
Giancarlo Ramirez <giancarloramirez5@gmail.com>
2024-09-13 23:54:05 -0700

    Delete Ch. 2 Prog Exercises directory

--- Ch. 2 Prog Exercises/program_ex_1.py
+++
@@ -1,7 +0,0 @@
-def info():
-    print("Giancarlo Ramirez")
-    print("7631 Reseda Blvd. Unit 40, Reseda, CA, 91334")
-    print("818-614-8634")
-    print("Computer Science and Engineering")
-    
-info()
